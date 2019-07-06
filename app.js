9cbbb84cba6c3433caf9dc6679cd54dbb2a29489
Juliano <mail@mail.com>
2019-07-06 20:29:10 -0300

    Inclusao de icone

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var monk = require('monk');
 var db = monk('localhost:27017/painel3x');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -30,7 +30,7 @@ app.use(function(req,res,next){
 });
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
